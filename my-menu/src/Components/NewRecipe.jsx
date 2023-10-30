49d03ae6ddb174eaecd78ceff28de19c3b74a35c
paula <paulalaorga@gmail.com>
2023-10-30 19:32:31 +1100

    RecipeForm and json server set up

--- my-menu/src/Components/NewRecipe.jsx
+++ my-menu/src/Components/NewRecipe.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import RecipeForm from "./RecipeForm";
 
-function YourComponent() {
+function NewRecipe() {
   const [showForm, setShowForm] = useState(false);
 
   const toggleForm = () => {
@@ -13,43 +14,10 @@ function YourComponent() {
         {showForm ? "Close Form" : "New Recipe"}
       </button>
       {showForm && (
-        <div className="container">
-          <div class="container">
-      <form class="add-recipe-form">
-        <h3>Add your recipe!</h3>
-
-        <input
-          type="text"
-          name="name"
-          value=""
-          placeholder="Enter a recipe name..."
-          class="input-text"
-        />
-        <br />
-        <input
-          type="text"
-          name="ingredients"
-          value=""
-          placeholder="Enter ingredients"
-          class="input-text"
-        />
-        <br />
-        <input
-          type="submit"
-          name="submit"
-          value="Create Recipe"
-          class="submit"
-        />
-      </form>
-    </div>
-    <p>
-      Create your own menu! <button id="new-recipe-btn">Add a new recipe!</button>
-    </p>
-          <button onClick={toggleForm}>Close Form</button>
-        </div>
+      <RecipeForm />
       )}
     </div>
   );
 }
 
-export default YourComponent;
+export default NewRecipe;
---
+++ my-menu/src/Components/RecipeForm.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+export default function RecipeForm() {
+  const [recipeName, setRecipeName] = useState("");
+  const [recipeIngredients, setRecipeIngredients] = useState("");
+
+  const handleRecipeNameChange = (event) => {
+    setRecipeName(event.target.value);
+  };
+
+  const handleRecipeIngredientsChange = (event) => {
+    setRecipeIngredients(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Create a JSON object with the data.
+    const recipeData = {
+      name: recipeName,
+      ingredients: recipeIngredients.split("\n").map((ingredient) => ingredient.trim()),
+    };
+
+    // POST the JSON data to a file or server (replace with your implementation).
+    fetch(" http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(recipeData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Recipe data has been saved:", data);
+      })
+      .catch((error) => {
+        console.error("Error saving recipe data:", error);
+      });
+  };
+
+  return (
+    <form className="add-recipe-form" onSubmit={handleSubmit}>
+      <label className="input-text">
+        Recipe name:
+        <input
+          className="input-text"
+          type="text"
+          name="recipe-name"
+          value={recipeName}
+          onChange={handleRecipeNameChange}
+        />
+        Ingredients:
+        <textarea
+          className="input-text"
+          name="recipe-ingredients"
+          value={recipeIngredients}
+          onChange={handleRecipeIngredientsChange}
+        />
+      </label>
+      <input className="submit" type="submit" value="Submit" />
+    </form>
+  );
+}
